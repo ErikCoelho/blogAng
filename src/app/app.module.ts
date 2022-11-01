9582cb1d9687fdf9a568e8db239fe52245d5a5ed
ErikCoelho <erikcoelhomoraes84@gmail.com>
2022-11-01 12:53:10 -0300

    Adicionando Pages e configurando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     NavbarComponent,
-    PostsComponent
+    PostsComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
