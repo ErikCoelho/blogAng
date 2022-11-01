d98bd31e67c1f48054ddcf199c6c8b0d7e6e8490
ErikCoelho <erikcoelhomoraes84@gmail.com>
2022-11-01 12:48:16 -0300

    Adicionando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
